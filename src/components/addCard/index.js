3a3daad6df39a801f05124716bbef726e6baf443
Tulio <tutex1500@gmail.com>
2020-04-30 20:23:49 -0300

    Foto dos jogadores

--- src/components/addCard/index.js
+++ src/components/addCard/index.js
@@ -6,6 +6,7 @@ export default class AddCard extends Component {
     return (
       <div className="card addCard">
         <h1>Adicionar carta ausente</h1>
+        <div id="jogadores">Selecionar jogador</div>
         <div id="cores">Selecionar cor</div>
         <div id="selectCard">Selecionar carta</div>
         <button className="preenchido">Adicionar</button>
