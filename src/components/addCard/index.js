578ad36311c00a538863d80869630952cb393a0e
Tulio <tutex1500@gmail.com>
2020-04-30 20:18:26 -0300

    Estilização das fontes

--- src/components/addCard/index.js
+++ src/components/addCard/index.js
@@ -4,7 +4,12 @@ import './styles.css';
 export default class AddCard extends Component {
   render() {
     return (
-      <div className="card addCard">AddCard</div>
+      <div className="card addCard">
+        <h1>Adicionar carta ausente</h1>
+        <div id="cores">Selecionar cor</div>
+        <div id="selectCard">Selecionar carta</div>
+        <button className="preenchido">Adicionar</button>
+      </div>
     );
   }
 }
