56f64ed8095baf1a1108b160b42df4b12c3aee0e
Tulio <tutex1500@gmail.com>
2020-05-02 16:47:13 -0300

    Ajustes na estilização

--- src/components/winner/index.js
+++ src/components/winner/index.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import './styles.css';
+
+import andre from '../../img/players/andre.jfif';
+import daniel from '../../img/players/daniel.jfif';
+import dumbo from '../../img/players/dumbo.jfif';
+import sara from '../../img/players/sara.jfif';
 
 export default class Winner extends Component {
   render() {
     return (
-      <div>Winner</div>
+      <div id="winner">
+        <img src={andre} alt="Jogador" className="circular playerPhoto"></img>
+        <img src={daniel} alt="Jogador" className="circular playerPhoto"></img>
+        <img src={dumbo} alt="Jogador" className="circular playerPhoto"></img>
+        <img src={sara} alt="Jogador" className="circular playerPhoto"></img>
+      </div>
     );
   }
 }
