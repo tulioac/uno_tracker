e873936ab7fc77cbddb5a407d5024f8a57ac8a7e
Tulio <tutex1500@gmail.com>
2020-05-02 22:18:27 -0300

    Seleção das opções para adição da carta

--- src/components/addCard/index.js
+++ src/components/addCard/index.js
@@ -19,17 +19,27 @@ export default class AddCard extends Component {
 
   selectPlayer = (player) => {
     this.setState({ player });
-    console.log(this.state.player);
   }
 
   selectColor = (color) => {
     this.setState({ color });
-    console.log(this.state.color);
   }
 
   selectCard = (card) => {
     this.setState({ card });
-    console.log(this.state.card);
+  }
+
+  addCardToPlayer = () => {
+    console.log(this.state);
+    // Enviar dados e depois resetar seleções
+
+
+
+    this.setState({
+      player: "",
+      color: "",
+      card: ""
+    })
   }
 
   render() {
@@ -37,33 +47,33 @@ export default class AddCard extends Component {
       <div className="card addCard">
         <h1>Adicionar carta ausente</h1>
         <div id="jogadores" className="espacado">
-          <img src={andre} alt="Andre" className="circular" onClick={() => this.selectPlayer("andre")}></img>
-          <img src={daniel} alt="Daniel" className="circular" onClick={() => this.selectPlayer("daniel")}></img>
-          <img src={dumbo} alt="Dumbo" className="circular" onClick={() => this.selectPlayer("dumbo")}></img>
-          <img src={sara} alt="Sara" className="circular" onClick={() => this.selectPlayer("sara")}></img>
+          <img src={andre} alt="Andre" className={`circular ${this.state.player === "andre" ? "selecionado" : ""}`} onClick={() => this.selectPlayer("andre")}></img>
+          <img src={daniel} alt="Daniel" className={`circular ${this.state.player === "daniel" ? "selecionado" : ""}`} onClick={() => this.selectPlayer("daniel")}></img>
+          <img src={dumbo} alt="Dumbo" className={`circular ${this.state.player === "dumbo" ? "selecionado" : ""}`} onClick={() => this.selectPlayer("dumbo")}></img>
+          <img src={sara} alt="Sara" className={`circular ${this.state.player === "sara" ? "selecionado" : ""}`} onClick={() => this.selectPlayer("sara")}></img>
         </div>
         <div id="cores" className="espacado">
-          <img src={blue} alt="Cor azul" className="circular" onClick={() => this.selectColor("blue")}></img>
-          <img src={red} alt="Cor vermelha" className="circular" onClick={() => this.selectColor("red")}></img>
-          <img src={yellow} alt="Cor amarela" className="circular" onClick={() => this.selectColor("yellow")}></img>
-          <img src={green} alt="Cor verde" className="circular" onClick={() => this.selectColor("green")}></img>
+          <img src={blue} alt="Cor azul" className={`circular ${this.state.color === "blue" ? "selecionado" : ""}`} onClick={() => this.selectColor("blue")}></img>
+          <img src={red} alt="Cor vermelha" className={`circular ${this.state.color === "red" ? "selecionado" : ""}`} onClick={() => this.selectColor("red")}></img>
+          <img src={yellow} alt="Cor amarela" className={`circular ${this.state.color === "yellow" ? "selecionado" : ""}`} onClick={() => this.selectColor("yellow")}></img>
+          <img src={green} alt="Cor verde" className={`circular ${this.state.color === "green" ? "selecionado" : ""}`} onClick={() => this.selectColor("green")}></img>
         </div>
         <div id="selectCard">
-          <img src={zero} alt="Zero" className="carta" onClick={() => this.selectCard("zero")}></img>
-          <img src={one} alt="Um" className="carta" onClick={() => this.selectCard("one")}></img>
-          <img src={two} alt="Dois" className="carta" onClick={() => this.selectCard("two")}></img>
-          <img src={three} alt="Três" className="carta" onClick={() => this.selectCard("three")}></img>
-          <img src={four} alt="Quatro" className="carta" onClick={() => this.selectCard("four")}></img>
-          <img src={five} alt="Cinco" className="carta" onClick={() => this.selectCard("five")}></img>
-          <img src={six} alt="Seis" className="carta" onClick={() => this.selectCard("six")}></img>
-          <img src={seven} alt="Sete" className="carta" onClick={() => this.selectCard("seven")}></img>
-          <img src={eight} alt="Oito" className="carta" onClick={() => this.selectCard("eight")}></img>
-          <img src={nine} alt="Nove" className="carta" onClick={() => this.selectCard("nine")}></img>
-          <img src={plus} alt="Plus" className="carta" onClick={() => this.selectCard("plus")}></img>
-          <img src={reverse} alt="Reverse" className="carta" onClick={() => this.selectCard("reverse")}></img>
-          <img src={skip} alt="Skip" className="carta" onClick={() => this.selectCard("skip")}></img>
+          <img src={zero} alt="Zero" className={`carta ${this.state.card === "zero" ? "selecionado" : ""}`} onClick={() => this.selectCard("zero")}></img>
+          <img src={one} alt="Um" className={`carta ${this.state.card === "one" ? "selecionado" : ""}`} onClick={() => this.selectCard("one")}></img>
+          <img src={two} alt="Dois" className={`carta ${this.state.card === "two" ? "selecionado" : ""}`} onClick={() => this.selectCard("two")}></img>
+          <img src={three} alt="Três" className={`carta ${this.state.card === "three" ? "selecionado" : ""}`} onClick={() => this.selectCard("three")}></img>
+          <img src={four} alt="Quatro" className={`carta ${this.state.card === "four" ? "selecionado" : ""}`} onClick={() => this.selectCard("four")}></img>
+          <img src={five} alt="Cinco" className={`carta ${this.state.card === "five" ? "selecionado" : ""}`} onClick={() => this.selectCard("five")}></img>
+          <img src={six} alt="Seis" className={`carta ${this.state.card === "six" ? "selecionado" : ""}`} onClick={() => this.selectCard("six")}></img>
+          <img src={seven} alt="Sete" className={`carta ${this.state.card === "seven" ? "selecionado" : ""}`} onClick={() => this.selectCard("seven")}></img>
+          <img src={eight} alt="Oito" className={`carta ${this.state.card === "eight" ? "selecionado" : ""}`} onClick={() => this.selectCard("eight")}></img>
+          <img src={nine} alt="Nove" className={`carta ${this.state.card === "nine" ? "selecionado" : ""}`} onClick={() => this.selectCard("nine")}></img>
+          <img src={plus} alt="Plus" className={`carta ${this.state.card === "plus" ? "selecionado" : ""}`} onClick={() => this.selectCard("plus")}></img>
+          <img src={reverse} alt="Reverse" className={`carta ${this.state.card === "reverse" ? "selecionado" : ""}`} onClick={() => this.selectCard("reverse")}></img>
+          <img src={skip} alt="Skip" className={`carta ${this.state.card === "skip" ? "selecionado" : ""}`} onClick={() => this.selectCard("skip")}></img>
         </div>
-        <button className="preenchido">Adicionar</button>
+        <button className="preenchido" onClick={this.addCardToPlayer}>Adicionar</button>
       </div>
     );
   }
