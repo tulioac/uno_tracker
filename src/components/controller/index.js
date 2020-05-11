a49004651d9b64730d4ed9e8fd1c53b02e0df3e3
Tulio <tutex1500@gmail.com>
2020-05-11 19:13:51 -0300

    Winner vindo do data

--- src/components/controller/index.js
+++ src/components/controller/index.js
@@ -6,19 +6,10 @@ import Winner from '../winner';
 import AddCard from '../addCard';
 import Stats from '../stats';
 
-import players from '../../data/data.json';
+import players from '../../data';
 
 export default class Controller extends Component {
-
-  // TODO: Receber informações do JSON
-
-  state = {
-    "andré": [],
-    "daniel": [],
-    "dumbo": [],
-    "sara": [],
-    "malu": [],
-  }
+  state = { players };
 
   addCardToPlayer = (cardInfo) => {
     const { player, card, color } = cardInfo;
@@ -29,15 +20,32 @@ export default class Controller extends Component {
   }
 
   render() {
+
+    console.log(this.state);
+
+    /*  
+    const winner = <Winner />;
+    const addCard = <AddCard players={players} addCardToPlayer={this.addCardToPlayer.bind(this)} />
+    const stats = <Stats players={players} playersCards={this.state} /> 
+    */
+
     return (
       <div id="game">
+        <img src={Logosvg} id="logo" alt="Logo"></img>
+        <Winner players={players} />
+        <div className="cards">
+          <AddCard players={players} />
+        </div>
+      </div>
+    );
+  }
+}
+
+{/* <div id="game">
         <img src={Logosvg} id="logo" alt="Logo"></img>
         <Winner />
         <div className="cards">
           <AddCard players={players} addCardToPlayer={this.addCardToPlayer.bind(this)} />
           <Stats players={players} playersCards={this.state} />
         </div>
-      </div>
-    );
-  }
-}
+</div> */}
---
+++ src/data/index.js
@@ -0,0 +1,40 @@
+import fotos from '../components/fotos';
+
+const dumbo = {
+  "name": "dumbo",
+  "wins": 38,
+  "cards": [],
+  "picture": fotos.dumbo
+}
+
+const sara = {
+  "name": "sara",
+  "wins": 36,
+  "cards": [],
+  "picture": fotos.sara
+}
+
+const andré = {
+  "name": "andré",
+  "wins": 22,
+  "cards": [],
+  "picture": fotos.andré
+}
+
+const daniel = {
+  "name": "daniel",
+  "wins": 21,
+  "cards": [],
+  "picture": fotos.daniel
+}
+
+const malu = {
+  "name": "malu",
+  "wins": 4,
+  "cards": [],
+  "picture": fotos.malu
+}
+
+const players = [dumbo, sara, andré, daniel, malu];
+
+export default players;
