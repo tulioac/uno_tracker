36a7a914e25f7f20896dbb5c43e98385cdb7ffe4
Tulio <tutex1500@gmail.com>
2020-05-12 12:53:14 -0300

    Adição do Jogador Túlio

--- src/components/fotos/index.js
+++ src/components/fotos/index.js
@@ -3,7 +3,8 @@ import daniel from '../../img/players/daniel.jfif';
 import dumbo from '../../img/players/dumbo.jfif';
 import sara from '../../img/players/sara.jfif';
 import malu from '../../img/players/malu.jfif';
+import túlio from '../../img/players/tulio.jfif';
 
-const fotos = { andré, daniel, dumbo, sara, malu };
+const fotos = { andré, daniel, dumbo, sara, malu, túlio };
 
 export default fotos;
--- src/data/index.js
+++ src/data/index.js
@@ -1,40 +1,47 @@
 import fotos from '../components/fotos';
 
 const dumbo = {
-  "name": "dumbo",
+  "name": "Dumbo",
   "wins": 38,
   "cards": [],
   "picture": fotos.dumbo
 }
 
 const sara = {
-  "name": "sara",
+  "name": "Sara",
   "wins": 36,
   "cards": [],
   "picture": fotos.sara
 }
 
+const túlio = {
+  "name": "Túlio",
+  "wins": 34,
+  "cards": [],
+  "picture": fotos.túlio
+}
+
 const andré = {
-  "name": "andré",
+  "name": "André",
   "wins": 22,
   "cards": [],
   "picture": fotos.andré
 }
 
 const daniel = {
-  "name": "daniel",
+  "name": "Daniel",
   "wins": 21,
   "cards": [],
   "picture": fotos.daniel
 }
 
 const malu = {
-  "name": "malu",
+  "name": "Malu",
   "wins": 4,
   "cards": [],
   "picture": fotos.malu
 }
 
-const players = [dumbo, sara, andré, daniel];
+const players = [dumbo, sara, túlio, andré, daniel];
 
 export default players;
